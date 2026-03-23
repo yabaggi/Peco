276185b7c2f6f2e7fd95a5df716f5f1c2c712a1c
yabaggi <yabaggi@gmail.com>
2026-03-23 21:55:38 +0400

    .

---
+++ cr.sh
@@ -0,0 +1,7 @@
+echo "# Peco" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yabaggi/Peco.git
+git push -u origin main
